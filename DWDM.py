aa2c850c6cf0d52ebbb2b840e9c7b1850f8393a8
LuisBarreno <lmbarreno2@outlook.com>
2021-01-24 12:56:35 -0500

    Add python files

---
+++ DWDM.py
@@ -0,0 +1,45 @@
+import openpyxl
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def process(sheet_obj, sheet_name):
+    G = nx.Graph()
+
+    # Will print a particular row value
+
+    for j in range(2, sheet_obj.max_row + 1):
+        source = sheet_obj.cell(row=j, column=2)
+        target = sheet_obj.cell(row=j, column=3)
+
+        if source.value:
+            G.add_node(source.value)
+
+        if target.value:
+            G.add_node(target.value)
+
+        if source.value and target.value and \
+                source.value != target.value and \
+                not G.has_edge(source.value, target.value) and \
+                not G.has_edge(target.value, source.value):
+            G.add_edge(source.value, target.value)
+
+        # print(row)
+    plt.figure(3, figsize=(50, 50))
+    nx.draw_networkx(G, arrowsize=6, with_labels=True, node_size=10, font_size=5)
+    plt.savefig(sheet_name + ".pdf")
+
+
+def main():
+    loc = 'RED DWDM.xlsx'
+    wb_obj = openpyxl.load_workbook(loc)
+    print(wb_obj.sheetnames)
+
+    for sheet_name in wb_obj.sheetnames:
+        process(wb_obj[sheet_name], sheet_name)
+
+
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
